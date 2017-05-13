9d2a31546bc8e5e3c77ec7280188b237f2035e53
depippo <jdepippo@gmail.com>
2017-05-12 23:03:13 -0400

    Done.

--- components/PoemWriter.js
+++ components/PoemWriter.js
@@ -1,17 +1,44 @@
 const React = require('react');
 
+function countWords(line) {
+  return line.split(' ').filter(l => l).length;
+}
+
+function validatePoem(poem) {
+  const lines = poem.split('\n').filter(l => l);
+  const correctLineCount = lines.length === 3;
+  const correctWordCount = countWords(lines[0]) === 5 && countWords(lines[1]) === 3 && countWords(lines[2]) === 5;
+  return correctLineCount && correctWordCount;
+}
+
 class PoemWriter extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      content: '',
+      isValid: false,
+    };
+
+    this.updatePoem = this.updatePoem.bind(this);
+  }
+
+  updatePoem(event) {
+    const content = event.target.value;
+
+    if (content) {
+      this.setState({
+        content: content,
+        isValid: validatePoem(content),
+      });
+    }
   }
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div>
+        <textarea rows="3" cols="60" value={this.state.content} onChange={this.updatePoem} />
+        {!this.state.isValid ? <div id="poem-validation-error" style={{color: 'red'}}>This poem is not written in the right structure!</div> : null}
       </div>
     );
   }
--- components/TwitterMessage.js
+++ components/TwitterMessage.js
@@ -4,17 +4,36 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      value: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(ev) {
+    this.setState({
+      value: ev.target.value,
+    });
   }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" />
+        <input type="text" value={this.state.value} onChange={this.handleChange}/>
+        <p>{this.props.maxChars - this.state.value.length} characters remanining.</p>
       </div>
     );
   }
+}
+
+TwitterMessage.propTypes = {
+  maxChars: React.PropTypes.number,
+}
+
+TwitterMessage.defaultProps = {
+  maxChars: 140,
 }
 
 module.exports = TwitterMessage;
